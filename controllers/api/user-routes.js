99011ff237228d4e465cea2d51bfc01aaa85e286
chrispobrien <chris.obrien57@gmail.com>
2022-07-05 06:35:06 -0400

    14.2.6

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,7 +64,17 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        // save uses a callback function
+        req.session.save(() => {
+            // Set session data
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            // Respond with user data
+            res.json(dbUserData);
+        })
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -88,8 +98,27 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
-        res.json({ user: dbUserData, message: 'You are now logged in! '});
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!'});
+        });
     });
+});
+
+// POST logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    };
 });
 
 // PUT /api/users/1
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,19 +36,14 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
 
-
-    // res.render('homepage', {
-    //     id: 1,
-    //     post_url: 'https://handlebarsjs.com/guide/',
-    //     title: 'Handlebars Docs',
-    //     created_at: new Date(),
-    //     vote_count: 10,
-    //     comments: [{}, {}],
-    //     user: {
-    //         username: 'test_user'
-    //     }
-    // });
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    };
+    res.render('login');
 });
 
 module.exports = router;
